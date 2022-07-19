b55fd4d0f9b8a03a8e447961f2e5acab0e1f5066
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-07-19 16:35:55 -0300

    feat: Add constructor and member array getter

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-contract MultisigWallet {}
+contract MultisigWallet {
+    address[] public members;
+    uint256 public immutable requiredApprovals;
+
+    constructor(address[] memory _members, uint256 _requiredApprovals) {
+        require(
+            _requiredApprovals <= _members.length,
+            "Required approvals should not be greater than the amount of members"
+        );
+
+        members = _members;
+        requiredApprovals = _requiredApprovals;
+    }
+
+    function getMembers() public view returns (address[] memory) {
+        return members;
+    }
+}
