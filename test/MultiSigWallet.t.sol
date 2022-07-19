361ec742a9a84228b2649773f4db2db8240f9d13
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-07-19 16:35:59 -0300

    test: Add constructor related tests

--- test/MultiSigWallet.t.sol
+++ test/MultiSigWallet.t.sol
@@ -3,6 +3,34 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
 
-contract MultisigWallet is Test {
+import "../src/MultisigWallet.sol";
+
+contract MultisigWalletTest is Test {
     function setUp() public {}
+
+    function testConstructorParametersLoaded() public {
+        uint256 memberCount = 10;
+        uint256 requiredApprovals = 2;
+        address[] memory members = new address[](memberCount);
+
+        for (uint160 i = 0; i < memberCount; i++) {
+            members[i] = address(i);
+        }
+
+        MultisigWallet multisigWallet =
+            new MultisigWallet(members, requiredApprovals);
+
+        assertEq(multisigWallet.getMembers(), members);
+        assertEq(multisigWallet.requiredApprovals(), requiredApprovals);
+    }
+
+    function testCannotRequireMoreApprovalsThanMembers() public {
+        uint256 requiredApprovals = 2;
+        address[] memory members = new address[](requiredApprovals - 1);
+
+        vm.expectRevert(
+            "Required approvals should not be greater than the amount of members"
+        );
+        new MultisigWallet(members, requiredApprovals);
+    }
 }
