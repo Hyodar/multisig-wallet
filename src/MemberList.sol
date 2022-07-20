c0d8f5a60881f95e0194c25ad1240e5b8126641c
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-07-20 14:57:36 -0300

    refac: Rename list to self to follow library naming standard

--- src/MemberList.sol
+++ src/MemberList.sol
@@ -14,107 +14,107 @@ library MemberList {
 
     /// @notice Checks whether an account is a member or not
     /// @dev Uses an underlying map in order to do this, so no O(n) required
-    /// @param list The List instance
+    /// @param self The List instance
     /// @param account The account address to be checked
-    function contains(List storage list, address account)
+    function contains(List storage self, address account)
         internal
         view
         returns (bool)
     {
-        return list._memberOrder[account] != 0;
+        return self._memberOrder[account] != 0;
     }
 
     /// @notice Adds an account address to the member list
     /// @dev The account must not be a member and must not be the zero address
-    /// @param list The List instance
+    /// @param self The List instance
     /// @param account The account address to be added
-    function add(List storage list, address account) internal {
-        require(!contains(list, account), "Account is already a member");
+    function add(List storage self, address account) internal {
+        require(!contains(self, account), "Account is already a member");
         require(account != address(0), "Zero address can't be added as member");
 
-        list._members.push(account);
-        list._memberOrder[account] = list._members.length;
+        self._members.push(account);
+        self._memberOrder[account] = self._members.length;
     }
 
     /// @notice Removes an account address from the member list
     /// @dev The account must be a member
-    /// @param list The List instance
+    /// @param self The List instance
     /// @param account The account address to be removed
-    function remove(List storage list, address account) internal {
-        uint256 removedMemberOrder = list._memberOrder[account];
+    function remove(List storage self, address account) internal {
+        uint256 removedMemberOrder = self._memberOrder[account];
 
         require(removedMemberOrder != 0, "Account is not a member");
 
-        uint256 memberCount = list._members.length;
+        uint256 memberCount = self._members.length;
 
         if (removedMemberOrder != memberCount) {
             address lastMember;
 
             unchecked {
                 // there is at least one member, memberCount > 0
-                lastMember = list._members[memberCount - 1];
+                lastMember = self._members[memberCount - 1];
 
                 // removedMemberOrder > 0
-                list._members[removedMemberOrder - 1] = lastMember;
+                self._members[removedMemberOrder - 1] = lastMember;
             }
 
-            list._memberOrder[lastMember] = removedMemberOrder;
+            self._memberOrder[lastMember] = removedMemberOrder;
         }
 
-        list._members.pop();
-        delete list._memberOrder[account];
+        self._members.pop();
+        delete self._memberOrder[account];
     }
 
     /// @notice Replaces an account address in the member list with another
     ///     address
     /// @dev The `from` account must be a member and the `to` account must not
     ///     be a member and must not be the zero address
-    /// @param list The List instance
+    /// @param self The List instance
     /// @param from The current member to be replaced
     /// @param to The non-member that will replace `from`
-    function replace(List storage list, address from, address to) internal {
-        require(!contains(list, to), "Account is already a member");
+    function replace(List storage self, address from, address to) internal {
+        require(!contains(self, to), "Account is already a member");
         require(to != address(0), "Zero address can't be added as member");
 
-        uint256 replacedMemberOrder = list._memberOrder[from];
+        uint256 replacedMemberOrder = self._memberOrder[from];
         require(replacedMemberOrder != 0, "Replaced account is not a member");
 
         unchecked {
             // replacedMemberOrder > 0
-            list._members[replacedMemberOrder - 1] = to;
+            self._members[replacedMemberOrder - 1] = to;
         }
 
-        list._memberOrder[to] = replacedMemberOrder;
-        delete list._memberOrder[from];
+        self._memberOrder[to] = replacedMemberOrder;
+        delete self._memberOrder[from];
     }
 
     /// @notice Gets the member at a specified position in the array
     /// @dev `index` must be less than the members list length
-    /// @param list The List instance
+    /// @param self The List instance
     /// @param index The index in the members array
-    function at(List storage list, uint256 index)
+    function at(List storage self, uint256 index)
         internal
         view
         returns (address)
     {
-        return list._members[index];
+        return self._members[index];
     }
 
     /// @notice Gets the underlying members array
     /// @dev Results in a (possibly large) array copy. Prefer {MemberList-at}
     ///     when possible.
-    /// @param list The List instance
-    function values(List storage list)
+    /// @param self The List instance
+    function values(List storage self)
         internal
         view
         returns (address[] memory)
     {
-        return list._members;
+        return self._members;
     }
 
     /// @notice Gets the amount of members
-    /// @param list The List instance
-    function length(List storage list) internal view returns (uint256) {
-        return list._members.length;
+    /// @param self The List instance
+    function length(List storage self) internal view returns (uint256) {
+        return self._members.length;
     }
 }
