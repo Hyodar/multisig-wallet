3b734cf2108cbc81ca1d96bb48ad2a3c629a2429
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-07-25 12:10:10 -0300

    feat: Update deployment script

--- script/MultiSigWallet.s.sol
+++ script/MultiSigWallet.s.sol
@@ -1,20 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.7;
 
 import "forge-std/Script.sol";
 import "../src/MultisigWallet.sol";
 
 contract MultisigWalletScript is Script {
     function run() public {
-        address[] memory members = new address[](5);
+        address[] memory members = new address[](3);
 
-        for (uint160 i = 0; i < members.length; i++) {
-            members[i] = address(i);
-        }
-
-        members[0] = msg.sender;
+        members[0] = 0x43Ebb448B90Ae5cfEBa16C3c315Ff486f8Ef3CB6;
+        members[1] = 0xa5563257778b3AAd74EaF50d17342f2fBF81437D;
+        members[2] = 0xf8290AdAeFef3DcC51A46F256c6cd560c75E7D94;
 
         vm.broadcast();
-        new MultisigWallet(members, 1);
+        new MultisigWallet(members, 2);
     }
 }
